48b3019224e18fe816ce70c531f0da3206c8bbb5
Tannian <Declan.Tannian@tranetechnologies.com>
2023-02-24 16:32:29 +0000

    topic 2 task Bank

---
+++ bank.py
@@ -0,0 +1,6 @@
+number1 = float(input('please enter amount 1 (in cent):'))
+number2 = float(input('please enter amount 2(in cent):'))
+number3   = (number1 + number2) / 100
+#cent = floatnum / 100
+
+print (f'the sum of these is {number3:.2f}')
