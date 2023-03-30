79f173663775cb37c382560173d264f1e5cd3f34
Tannian <Declan.Tannian@tranetechnologies.com>
2023-03-30 21:12:12 +0100

    exception handling added on es file, cleaner code than previous version of es.py

--- es.py
+++ es.py
@@ -1,18 +1,16 @@
- #https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
+#https://www.w3schools.com/python/trypython.asp?filename=demo_ref_string_count
+
+#using sys.arg as demonstrated in WK09 Lecture
+
+import sys
  
- # Program to get letter count in a text file
- 
-# explicit function to return the letter count
-def letterFrequency(fileName, letter):
-    # open file in read mode
-    file = open(fileName, 'r')
- 
-    # store content of the file in a variable
-    text = file.read()
- 
-    # using count()
-    return text.count(letter)
- 
- 
-# call the function and display the letter count
-print(letterFrequency('SampleFile.txt', 'e'))
+filename = sys.argv[1]  
+
+try:
+    with open(filename,'r') as f:
+       text = f.read()
+       count = text.count("e")
+       print(count)
+
+except FileNotFoundError:
+   print("file does not exist")
