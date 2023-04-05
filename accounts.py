04ba476c80a8cafa1202e5fb581834b52adc1f63
Tannian <Declan.Tannian@tranetechnologies.com>
2023-04-05 15:12:09 +0100

    updates to accounts.py, added while true, added break out of while loop, added comments to explain code

--- accounts.py
+++ accounts.py
@@ -3,17 +3,21 @@
 #https://www.geeksforgeeks.org/python-check-whether-string-contains-only-numbers-or-not/?ref=rp
 
 
-import sys
-
-try:
-  accountnum = str(input('Please enter an 10 digit account number:'))
-  if len(accountnum) != 10:
-     raise ValueError("Account number entered must be 10 digits")
-
-  if accountnum.isdigit():
-   maskednum = accountnum[-4:].rjust(len(accountnum),'*')
-   print(maskednum)
-  else:
-   print("you must enter numbers only")
-except ValueError as e:
-   print(e)
+while True :
+   try:
+      accountnum = str(input('Please enter an 10 digit account number:'))
+      if len(accountnum) != 10:
+         raise ValueError("Account number entered must be 10 digits")
+      #check if number entered is numerical
+      if accountnum.isdigit():
+         # replace last 4 digits of acountnumber  with * & format to right of string
+         maskednum = accountnum[-4:].rjust(len(accountnum),'*')
+         print(maskednum)
+         #print(accountnum)
+         #break while true
+         break
+         print("program ending")
+      else:
+         print("you must enter numbers only")
+   except ValueError as e:
+      print(e)
--- bank.py
+++ bank.py
@@ -7,10 +7,10 @@ while True:
         number1 = int(input('please enter amount 1 (in cent):'))
         number2 = int(input('please enter amount 2 (in cent):'))
         if number1 < 0 or number2 <0:
-            raise ValueError("Both first and second number must be positive, and must not contain any symbols")
+            raise ValueError
         break
     except ValueError as e:
-        print(e)
+        print("Both first and second number must be positive, and must not contain any symbols, ",e)
            #elif: number1 >0 and number2 >0  
 number3   = (number1 + number2) / 100
 print(f'the sum of these is €{number3:.2f}')
